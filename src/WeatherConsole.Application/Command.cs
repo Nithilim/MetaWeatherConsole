24d68447211133f3606ecaa91afa01811987c8fb
Paulius Kandratavičius <paulius.k@tesonet.com>
2019-10-09 22:06:31 +0300

    add command object and parser

---
+++ src/WeatherConsole.Application/Command.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherConsole.Application
+{
+    public class Command
+    {
+        public string Value { get; }
+    }
+}
---
+++ src/WeatherConsole.Application/CommandParser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherConsole.Application
+{
+    public class CommandParser
+    {
+        public static IEnumerable<Command> ParseCommands(List<string> args)
+        {
+
+        }
+    }
+}
