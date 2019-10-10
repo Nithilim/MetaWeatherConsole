f26df55df076245513bd04054e20b598b684f292
Paulius Kandratavičius <paulius.k@tesonet.com>
2019-10-10 16:45:25 +0300

    Add city command parser

--- src/WeatherConsole.Application/CommandParser.cs
+++ src/WeatherConsole.Application/CommandParser.cs
@@ -1,14 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace WeatherConsole.Application
 {
     public class CommandParser
     {
-        public static IEnumerable<Command> ParseCommands(string args)
+        public static IEnumerable<Command> ParseCommand(string args)
         {
-            if(args.Contains("--city"))
+            string command = "";
+            if(args != string.Empty)
+            {
+                if (args.StartsWith("--city"))
+                    command = "--city";
+
+                if(args.StartsWith("-c") && !args.StartsWith("-city"))
+                    command = "-c";
+
+                args.Remove(0, command.Length);
+            }
+
+            if (args.Contains("--city"))
+            {
+                var commandValues = args.Split(',').ToList();
+                var commands = commandValues.Select(i => new CityCommand(i));
+                return commands;
+            }
+
+            return new List<Command>();
         }
     }
 }
