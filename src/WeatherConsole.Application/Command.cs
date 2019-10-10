427fb93ea1768ac74394fa65515e5387e3e8dd5d
Paulius Kandratavičius <paulius.k@tesonet.com>
2019-10-10 18:54:05 +0300

    cleanup, add conditions to main

--- src/WeatherConsole.Application/Command.cs
+++ src/WeatherConsole.Application/Command.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace WeatherConsole.Application
+﻿namespace WeatherConsole.Application
 {
     public class Command
     {
--- src/WeatherConsole.Application/Program.cs
+++ src/WeatherConsole.Application/Program.cs
@@ -1,11 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace MetaWeatherConsole
+namespace WeatherConsole.Application
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string args)
         {
+            while(Keyboard.)
+            Console.WriteLine("Press ESC to exit");
+            PrepareCommands(args);
+        }
+
+        private static IEnumerable<Command> PrepareCommands(string args)
+        {
+            if (string.IsNullOrEmpty(args))
+            {
+                Console.WriteLine("Please enter command: ");
+                args = Console.ReadLine();
+            }
         }
     }
 }
