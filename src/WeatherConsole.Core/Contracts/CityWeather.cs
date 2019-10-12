7e9681c4d2e7e10d5c1d146361ec185b2bca9234
Paulius Kandratavičius <paulius.k@tesonet.com>
2019-10-12 16:28:58 +0300

    remove cities contract add cityweather contract

---
+++ src/WeatherConsole.Core/Contracts/CityWeather.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace WeatherConsole.Core.Contracts
+{
+    public class CityWeather
+    {
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        [JsonProperty("temperature")]
+        public double Temperature { get; set; }
+
+        [JsonProperty("precipitation")]
+        public int Precipitation { get; set; }
+
+        [JsonProperty("weather")]
+        public string Weather { get; set; }
+    }
+}
